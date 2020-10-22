fd946bc64d10194c32e8835817813f0e9dfc0708
thomasliebgott <thomas.liebgott@live.fr>
2020-10-22 17:44:05 +0200

    rendu chess 22/10

--- exercises/chess/model/piece.go
+++ exercises/chess/model/piece.go
@@ -1,3 +1,62 @@
 package model
 
 //Implement game pieces here
+type PionBlanc struct {
+	coor int
+	num  int
+}
+
+type PionNoir struct {
+	coor int
+	num  int
+}
+
+type CavalierBlanc struct {
+	coor int
+	num  int
+}
+
+type CavalierNoir struct {
+	coor int
+	num  int
+}
+
+type DameBlanche struct {
+	coor int
+	num  int
+}
+
+type DameNoir struct {
+	coor int
+	num  int
+}
+
+type RoiBlanc struct {
+	coor int
+	num  int
+}
+
+type RoiNoir struct {
+	coor int
+	num  int
+}
+
+type FouBlanc struct {
+	coor int
+	num  int
+}
+
+type FouNoir struct {
+	coor int
+	num  int
+}
+
+type TourBlanc struct {
+	coor int
+	num  int
+}
+
+type TourNoir struct {
+	coor int
+	num  int
+}
--- exercises/chess/model/state.go
+++ exercises/chess/model/state.go
@@ -1,3 +1,49 @@
 package model
 
-// Implement game state related elements here
+func MovePionBlanc(coor int, num int) PionBlanc {
+	PionBlanc := PionBlanc{coor, num}
+}
+
+func MovePionNoir(coor int, num int) PionNoir {
+	PionNoir := PionNoir{coor, num}
+}
+
+func MoveCavalierBlanc(coor int, num int) CavalierBlanc {
+	CavalierBlanc := CavalierBlanc{coor, num}
+}
+
+func MoveCavalierNoir(coor int, num int) CavalierNoir {
+	CavalierNoir := CavalierNoir{coor, num}
+}
+
+func MoveDameBlanche(coor int, num int) DameBlanche {
+	DameBlanche := DameBlanche{coor, num}
+}
+
+func MoveDameNoir(coor int, num int) DameNoir {
+	DameNoir := DameNoir{coor, num}
+}
+
+func MoveRoiBlanc(coor int, num int) RoiBlanc {
+	RoiBlanc := RoiBlanc{coor, num}
+}
+
+func MoveRoiNoir(coor int, num int) RoiNoir {
+	RoiNoir := RoiNoir{coor, num}
+}
+
+func MoveFouBlanc(coor int, num int) FouBlanc {
+	FouBlanc := FouBlanc{coor, num}
+}
+
+func MoveFouNoir(coor int, num int) FouNoir {
+	FouNoir := FouNoir{coor, num}
+}
+
+func MoveTourBlanc(coor int, num int) TourBlanc {
+	TourBlanc := TourBlanc{coor, num}
+}
+
+func MoveTourNoir(coor int, num int) TourNoir {
+	TourNoir := TourNoir{coor, num}
+}
